e7fe0e57fa9dae58cafb2de9e48fe51ca003766d
SoapyHoang <sxhoang@ucsd.edu>
2022-05-03 23:12:53 -0700

    finished math

--- js/math.js
+++ js/math.js
@@ -1,9 +1,20 @@
 //STEP 1
+// let num = prompt("Enter a number")
+// console.log(Math.abs(num))
 
 //STEP 2
+// let numFloat = prompt("Enter a decimal number")
+// console.log(Math.ceil(numFloat))
 
 //STEP 3
+// let numFloat = prompt("Enter a decimal number")
+// console.log(Math.floor(numFloat))
 
-//STEP 4
+// STEP 4
+// let nums = prompt("Enter 5 numbers for ex: (1,2,3,4,5)")
+// nums = nums.split(",").map((n) => {return parseInt(n)})
+// console.log("Max:", Math.max(...nums), "Min:", Math.min(...nums))
 
 //STEP 5
+let nums = prompt("Enter a number")
+console.log(Math.sqrt(nums))
